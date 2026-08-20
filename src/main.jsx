6ce4703e4346b45d3efe8326ef679fe8047ac479
Mahak Patel <mahakpateluiux@gmail.com>
2026-08-20 19:09:26 -0400

    load footer logo responsive styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,4 +5,5 @@ import './styles/globals.css'
 import './styles/components.css'
 import './styles/responsive.css'
 import './styles/brand-theme.css'
+import './styles/footer-logo-fix.css'
 createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>)
