bda7a2aab02874fa3ba14c7bdca44bdbafef8262
Mahak Patel <mahakpateluiux@gmail.com>
2026-08-20 20:22:45 -0400

    style: load course detail orange accent fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,4 +6,5 @@ import './styles/components.css'
 import './styles/responsive.css'
 import './styles/brand-theme.css'
 import './styles/footer-logo-fix.css'
+import './styles/course-detail-color-fix.css'
 createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>)
